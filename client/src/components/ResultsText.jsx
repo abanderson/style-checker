e2ac875620fd79ca8324a17ddc79a29b214858da
Alex Anderson <alex@abanderson.com>
2018-12-04 22:36:14 +0800

    Adjusted text copy element and styling

--- client/src/components/ResultsText.jsx
+++ client/src/components/ResultsText.jsx
@@ -7,10 +7,10 @@ const ResultsText = () => {
                 <div className="d-flex justify-content-between align-items-baseline">
                     <h5 className="d-inline">Results</h5>
 
-                    <a className="text-secondary mr-2 text-copy" href="#">
+                    <span className="mr-2 text-copy">
                         <span className="far fa-copy" />
-                        <small className="font-weight-light">Copy</small>
-                    </a>
+                        <small className="font-weight-light ml-1">Copy</small>
+                    </span>
                 </div>
             </div>
             <p>The secret to doing anything is believing that you can do it.</p>
