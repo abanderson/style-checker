2d3e619709f935caa24519d64ee6b2b3b12c633b
Alex Anderson <alex@abanderson.com>
2019-02-11 23:25:02 +0800

    Implemented class methods and Navbar props
    - Added constructor, getStyleRules and componentDidMount methods.
    - Implemented passing props to Navbar component containing the number of style rules loaded
    - Changed one instance of class to className

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import Axios from "axios";
 import Navbar from "./components/Navbar";
 import StyleChecker from "./components/StyleChecker";
 import Rules from "./components/Rules";
 import "./App.css";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            styleRules: [],
+            numStyleRules: 0
+        };
+    }
+
+    getStyleRules() {
+        Axios.get("/rules/")
+            .then(response => {
+                this.setState({
+                    styleRules: response.data,
+                    numStyleRules: response.data.length
+                });
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    componentDidMount() {
+        this.getStyleRules();
+    }
+
     render() {
         return (
             <div>
-                <Navbar />
-                <div class="container">
+                <Navbar numStyleRules={this.state.numStyleRules} />
+                <div className="container">
                     <Switch>
                         <Route exact path="/" component={StyleChecker} />
                         <Route exact path="/rules" component={Rules} />
