4b4088812c1ded5ea4d367d919e61f5f3ea0639d
Alex Anderson <alex@abanderson.com>
2019-02-15 19:11:43 +0800

    Implemented component

---
+++ client/src/components/AddRule.jsx
@@ -0,0 +1,181 @@
+import React, { Component } from "react";
+import Axios from "axios";
+
+class AddRule extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ruleName: "",
+            searchRegex: "",
+            displayText: "",
+            isCorrection: false,
+            correctionRegex: "",
+            ruleSource: "",
+            isEnabled: true
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    resetState() {
+        this.setState({
+            ruleName: "",
+            searchRegex: "",
+            displayText: "",
+            isCorrection: false,
+            correctionRegex: "",
+            ruleSource: "",
+            isEnabled: true
+        });
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value =
+            target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        Axios.post("http://localhost:3001/rules/", {
+            ruleName: this.state.ruleName,
+            searchRegex: this.state.searchRegex,
+            displayText: this.state.displayText,
+            isCorrection: this.state.isCorrection,
+            correctionRegex: this.state.correctionRegex,
+            ruleSource: this.state.ruleSource,
+            isEnabled: this.state.isEnabled
+        })
+            .then(response => {
+                console.log(response);
+                this.props.onAddRule();
+                this.resetState();
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    render() {
+        return (
+            <div className="row mt-3 mb-3 justify-content-center">
+                <div className="col-lg-8">
+                    <h3>Add Rule</h3>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="ruleName">Rule name</label>
+                            <input
+                                name="ruleName"
+                                type="text"
+                                className="form-control"
+                                id="ruleName"
+                                placeholder="Enter rule name"
+                                value={this.state.ruleName}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="searchRegex">
+                                Search expression
+                            </label>
+                            <input
+                                name="searchRegex"
+                                type="text"
+                                className="form-control"
+                                id="searchRegex"
+                                placeholder="Enter regular expression"
+                                value={this.state.searchRegex}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="displayText">Display text</label>
+                            <input
+                                name="displayText"
+                                type="text"
+                                className="form-control"
+                                id="displayText"
+                                placeholder="Enter display text"
+                                value={this.state.displayText}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="form-group form-check">
+                            <input
+                                name="isCorrection"
+                                type="checkbox"
+                                className="form-check-input"
+                                id="isCorrection"
+                                checked={this.state.isCorrection}
+                                onChange={this.handleChange}
+                            />
+                            <label
+                                className="form-check-label"
+                                htmlFor="isCorrection"
+                            >
+                                Correction available?
+                            </label>
+                        </div>
+                        <fieldset disabled={!this.state.isCorrection}>
+                            <div className="form-group">
+                                <label htmlFor="correctionRegex">
+                                    Correction expression
+                                </label>
+                                <input
+                                    name="correctionRegex"
+                                    type="text"
+                                    className="form-control"
+                                    id="correctionRegex"
+                                    placeholder="Enter regular expression"
+                                    value={this.state.correctionRegex}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                        </fieldset>
+                        <div className="form-group">
+                            <label htmlFor="ruleSource">Rule source</label>
+                            <input
+                                name="ruleSource"
+                                type="text"
+                                className="form-control"
+                                id="ruleSource"
+                                placeholder="Enter rule source"
+                                value={this.state.ruleSource}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="form-group form-check">
+                            <input
+                                name="isEnabled"
+                                type="checkbox"
+                                className="form-check-input"
+                                id="isEnabled"
+                                checked={this.state.isEnabled}
+                                onChange={this.handleChange}
+                            />
+                            <label
+                                className="form-check-label"
+                                htmlFor="isEnabled"
+                            >
+                                Rule enabled?
+                            </label>
+                        </div>
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AddRule;
