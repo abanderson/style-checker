416f44efa8d0bbae1b8cf28c7231256aee7425de
Alex Anderson <alex@abanderson.com>
2019-02-17 00:43:37 +0800

    Implemented display of individual rules

--- client/src/components/MatchedRule.jsx
+++ client/src/components/MatchedRule.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const MatchedRule = () => {
-    return <div>Matched Rule</div>;
+const MatchedRule = ({ matchedRule }) => {
+    return (
+        <li className="list-group-item">
+            <strong>{matchedRule.match[0]}</strong>
+            <br />
+            {matchedRule.matchedRuleDisplayText}
+            <br />
+            <small>{matchedRule.matchedRuleSource}</small>
+        </li>
+    );
 };
 
 export default MatchedRule;
