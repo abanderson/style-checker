505608d499fc8e17925c90253b4d52a3f5b8acbe
Alex Anderson <alex@abanderson.com>
2019-02-17 00:45:46 +0800

    Implemented matched rule counter and display

--- client/src/components/MatchedRules.jsx
+++ client/src/components/MatchedRules.jsx
@@ -1,28 +1,15 @@
 import React from "react";
+import MatchedRule from "./MatchedRule";
 
-const MatchedRules = () => {
+const MatchedRules = ({ ruleMatches }) => {
+    const matchedRules = ruleMatches.map((matchedRule, index) => {
+        return <MatchedRule key={index} matchedRule={matchedRule} />;
+    });
+
     return (
         <div className="correction-list">
-            <h5>5 rules matched</h5>
-            <ul className="list-group">
-                <li className="list-group-item">
-                    Possibly confused with <strong>Discrete</strong>
-                </li>
-                <li className="list-group-item">
-                    Change to third-person voice in press releases
-                </li>
-                <li className="list-group-item">
-                    Change to <strong>percent</strong> in narrative copy. Leave
-                    unchanged in headings.
-                </li>
-                <li className="list-group-item">
-                    Change to <strong>%</strong> in headings. Leave unchanged in
-                    narrative copy
-                </li>
-                <li className="list-group-item">
-                    Hyphenate (e.g., "stand-alone")
-                </li>
-            </ul>
+            <h5>{ruleMatches.length} matches found</h5>
+            <ul className="list-group">{matchedRules}</ul>
         </div>
     );
 };
