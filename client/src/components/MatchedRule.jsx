715af97602e9f3e8a41865449a5ad98d1dfce57d
Alex Anderson <alex@abanderson.com>
2019-02-18 12:11:56 +0800

    Converted to class and added mouse events

--- client/src/components/MatchedRule.jsx
+++ client/src/components/MatchedRule.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 
-const MatchedRule = ({ matchedRule }) => {
-    return (
-        <li className="list-group-item">
-            <strong>{matchedRule.match[0]}</strong>
-            <br />
-            {matchedRule.matchedRuleDisplayText}
-            <br />
-            <small>{matchedRule.matchedRuleSource}</small>
-        </li>
-    );
-};
+class MatchedRule extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
+    }
+
+    handleMouseOver() {
+        console.log(
+            `Mouse over rule: ${this.props.matchedRule.matchedRuleName}`
+        );
+        //  Don't do this: console.log(this.props.key);
+    }
+
+    handleMouseOut() {
+        console.log(
+            `Mouse out of rule: ${this.props.matchedRule.matchedRuleName}`
+        );
+    }
+
+    render() {
+        return (
+            <li
+                className="list-group-item"
+                onMouseOver={this.handleMouseOver}
+                onMouseOut={this.handleMouseOut}
+            >
+                <strong>{this.props.matchedRule.match[0]}</strong>
+                <br />
+                {this.props.matchedRule.matchedRuleDisplayText}
+                <br />
+                <small>{this.props.matchedRule.matchedRuleSource}</small>
+            </li>
+        );
+    }
+}
 
 export default MatchedRule;
