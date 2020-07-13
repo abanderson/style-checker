874be8547319c22e7f5567f54a53b3f31f56d826
Alex Anderson <alex@abanderson.com>
2020-07-13 18:18:51 +0800

    Implemented update rule method

--- routes/rules.js
+++ routes/rules.js
@@ -3,45 +3,63 @@ const router = express.Router();
 const models = require("../models");
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
     models.Rule.findAll()
-        .then(rules => {
+        .then((rules) => {
             res.json(rules);
         })
-        .catch(error => {
+        .catch((error) => {
             console.error(error);
             next(error);
         });
 });
 
-router.post("/", function(req, res, next) {
+router.post("/", function (req, res, next) {
     models.Rule.create({
         ruleName: req.body.ruleName,
         searchRegex: req.body.searchRegex,
         displayText: req.body.displayText,
         correctionRegex: req.body.correctionRegex,
         ruleSource: req.body.ruleSource,
-        isEnabled: req.body.isEnabled
+        isEnabled: req.body.isEnabled,
     })
-        .then(rule => {
+        .then((rule) => {
             res.json(rule);
         })
-        .catch(error => {
+        .catch((error) => {
             console.error(error);
             next(error);
         });
 });
 
-router.put("/:ruleId", function(req, res, next) {
-    // Complete
-});
-
-router.delete("/:ruleId", function(req, res, next) {
-    models.Rule.destroy({ where: { id: req.params.ruleId } })
-        .then(result => {
+router.put("/:ruleId", function (req, res, next) {
+    models.rule
+        .update(
+            {
+                ruleName: req.body.ruleName,
+                searchRegex: req.body.searchRegex,
+                displayText: req.body.displayText,
+                correctionRegex: req.body.correctionRegex,
+                ruleSource: req.body.ruleSource,
+                isEnabled: req.body.isEnabled,
+            },
+            { where: { id: req.params.ruleId } }
+        )
+        .then((result) => {
             res.json(result);
         })
-        .catch(error => {
+        .catch((error) => {
+            console.error(error);
+            next(error);
+        });
+});
+
+router.delete("/:ruleId", function (req, res, next) {
+    models.Rule.destroy({ where: { id: req.params.ruleId } })
+        .then((result) => {
+            res.json(result);
+        })
+        .catch((error) => {
             console.error(error);
             next(error);
         });
