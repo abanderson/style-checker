edfda6bc3a01d1124e0172275a465a17e8895afd
Alex Anderson <alex@abanderson.com>
2020-07-29 15:26:38 +0800

    Update for v2 design and refactoring

--- client/src/components/StyleChecker.jsx
+++ client/src/components/StyleChecker.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SourceText from "./SourceText";
 import Results from "./Results";
+import MatchedRules from "./MatchedRules";
 import PropTypes from "prop-types";
 
 class StyleChecker extends Component {
@@ -169,12 +170,16 @@ class StyleChecker extends Component {
 
     render() {
         return (
-            <div className="source-results-container">
-                <SourceText onTextInput={this.handleTextInput} />
-                <Results
+            <div className="main-container">
+                <div className="source-results-container">
+                    <SourceText onTextInput={this.handleTextInput} />
+                    <Results
+                        editedText={this.state.editedText}
+                        highlightedText={this.state.highlightedText}
+                    />
+                </div>
+                <MatchedRules
                     ruleMatches={this.state.ruleMatches}
-                    editedText={this.state.editedText}
-                    highlightedText={this.state.highlightedText}
                     setHighlightedText={this.handleHighlightedText}
                     setDismissedRule={this.handleDismissRule}
                     correctRule={this.handleCorrectRule}
