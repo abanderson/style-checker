e648c966c6a67e13bac478ae922a50fcec88cc12
Alex Anderson <alex@abanderson.com>
2021-03-17 18:17:36 +0800

    Implement static rendering of client build directory

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -22,6 +23,13 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.get('/', function(req, res, next) {
+    console.log("Hit!");
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 app.use("/rules", rulesRouter);
+
+app.listen(9000);
 
 module.exports = app;
